ba37ca76e036dd713fcd8a4e06d66ffc9f3bbb6a
ChristianCurtis <49933990+ChristianCurtis@users.noreply.github.com>
2024-01-26 08:47:25 -0700

    Update PortentaH7_X509.ino
    Cleaned up formatting and removed extraneous evaluation code.
    
    Signed-off-by: ChristianCurtis <49933990+ChristianCurtis@users.noreply.github.com>

--- PortentaH7_X509.ino
+++ PortentaH7_X509.ino
@@ -1,19 +1,4 @@
-//#define LIB_CLIENTSECURE
-#define LIB_BEARSSL
-//#define LIB_ESPSSLCLIENT
-//#define LIB_GVOROXSSLCLIENT
-
-#if defined (LIB_CLIENTSECURE)
-  #include <WiFiClientSecure.h>
-#elif defined (LIB_BEARSSL)
-  #include <ArduinoBearSSL.h>
-#elif defined (LIB_ESPSSLCLIENT)
-  #include <ESP_SSLClient.h>
-#elif defined (LIB_GVOROXSSLCLIENT)
-  #include "SSLClient.h"
-#else
-  #error "NO SSL LIBRARY SELECTED"
-#endif
+#include <ArduinoBearSSL.h>
 #include <ArduinoECCX08.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
@@ -26,11 +11,8 @@
 #include <Arduino_ConnectionHandler.h>
 #include <arduino-timer.h>
 
-
 WiFiConnectionHandler conMgr(SECRET_SSID, SECRET_PASS);
-
 StaticJsonDocument<200> doc;
-
 auto timer = timer_create_default(); // create a timer with default settings
 
 // NTP Time 
@@ -39,16 +21,8 @@ NTPClient timeClient(NTPUdp,"pool.ntp.org");
 
 // Clients
 WiFiClient wifiClient;
-#if defined (LIB_ESPSSLCLIENT)
-  ESP_SSLClient ssl_client;
-#elif defined (LIB_BEARSSL)
-  BearSSLClient ssl_client(wifiClient);
-#else
-  #error "NO SSL LIBRARY SELECTED"
-#endif
-
+BearSSLClient ssl_client(wifiClient);
 MqttClient mqtt_client(ssl_client);
-
 
 void setup() {
   Serial.begin(115200);
@@ -56,52 +30,27 @@ void setup() {
     delay(1000);
   }
 
-    if (!ECCX08.begin()) {
-    Serial.println("No ECCX08 present!");
-    while (1);
+  if (!ECCX08.begin()) {
+      Serial.println("No ECCX08 present!");
+      while (1);
   }
-
   pinMode(LED_BUILTIN, OUTPUT); // set LED pin to OUTPUT
-  
-#if defined (LIB_ESPSSLCLIENT)
-  // Init SSL with certificates and keys
-  Serial.println("Using ESPSSL CLIENT");
-  ssl_client.setClient(&wifiClient);
-  ssl_client.setCACert(SECRET_ROOTCERT);
-  ssl_client.setCertificate(SECRET_DEVCERT);
-  ssl_client.setPrivateKey(SECRET_PRIKEY);
-  ssl_client.setDebugLevel(3);  // 0=none; 1=error; 2=warn; 3=info; 4=dump
-  // Set the receive and transmit buffers size in bytes for memory allocation (512 to 16384).
-  ssl_client.setBufferSizes(1024 /* rx */, 512 /* tx */);
-
-
-
-
-#elif defined (LIB_BEARSSL)
+    
   Serial.println("Using BEARSSL CLIENT");
   ArduinoBearSSL.onGetTime(getTime);
-  // Note how the certificate is formed
-//  String(certificate) = String(SECRET_DEVCERT) + String(SECRET_ROOTCERT);
-   
-   // Set the ECCX08 slot to use for the private key
-  // and the accompanying public certificate for it
-//  ssl_client.setEccSlot(0, certificate.c_str());
-
   ssl_client.setKey(SECRET_PRIKEY, SECRET_DEVCERT);
- 
+  
   timeClient.begin();
-
-#endif 
-
+  
   // Init MQTT client
   mqtt_client.setId(SECRET_CLIENT_ID);
   mqtt_client.onMessage(onMQTTMessage);
-
+  
   // Connection Manager callbacks
   conMgr.addCallback(NetworkConnectionEvent::CONNECTED, onNetworkConnect);
   conMgr.addCallback(NetworkConnectionEvent::DISCONNECTED, onNetworkDisconnect);
   conMgr.addCallback(NetworkConnectionEvent::ERROR, onNetworkError);
-  
+    
   // JSON Payload formatting
   // Refer to the IoTConnect D2C documentation for formatting 
   JsonObject data = doc["d"][0].createNestedObject("d");      
@@ -109,9 +58,7 @@ void setup() {
   data["text"] = "HELLO FROM H7!!";
 
   timer.every(20000, publishData);  
-    
 }
-
 
 void loop() {
   mqtt_client.poll();
@@ -128,7 +75,6 @@ void loop() {
     // Serial.println("NetworkConnectionState not Connected");
     ;
   }
-
 }
 
  // =================== Networking Handlers ======================================
@@ -142,27 +88,9 @@ void onNetworkConnect() {
   Serial.println("Fetching network time");
   timeClient.forceUpdate();
 
-#if defined (LIB_ESPSSLCLIENT)
-  
-  if(!timeClient.isTimeSet())
-  {
-    Serial.println("Failed:  No time set.  Program Halted");
-    while(1)
-      ;
-  }
-  else{
-    Serial.print("Time set to: ");
-    Serial.println(timeClient.getFormattedTime());
-    ssl_client.setX509Time(timeClient.getEpochTime());
-  }
-
-#elif defined (LIB_BEARSSL)
   unsigned long mytime = getTime();
   Serial.print("Time set to: ");
   Serial.println(mytime);
-
-#endif
-
 }
 
 
@@ -185,11 +113,7 @@ void connectMQTT(){
       Serial.print("MQTT Error= ");
       Serial.println(mqtt_client.connectError());
       Serial.print("SSL Error= ");
-  #if defined (LIB_ESPSSLCLIENT)
-      Serial.println(ssl_client.getLastSSLError());
-  #elif defined (LIB_BEARSSL)
       Serial.println(ssl_client.errorCode());
-  #endif
       Serial.println(getTime());
       delay(3000);
   }
@@ -240,7 +164,5 @@ bool toggle_led(void *) {
 }
 
 unsigned long getTime() {
-  // get the current time from the WiFi module  
   return timeClient.getEpochTime();
-//return WiFi.getTime();
 }
